a166457e0fe593db843633bc801c683419c0f7c6
renan-feitosa <renan.oliveira@sou.inteli.edu.br>
2024-09-12 11:31:11 -0300

    feat: add post endpoints

--- src/server/Core/app.py
+++ src/server/Core/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import re
+from flask import Flask, request, jsonify
 import os
 from supabase import create_client, Client
 from dotenv import load_dotenv
@@ -21,8 +22,8 @@ def games():
     response = supabase.table('game').select("*").execute()
     return response.data
 
-@app.route('/leaderboard')
-def leaderboard():
+@app.route('/scores')
+def scores():
 
     today = datetime.now().strftime('%Y-%m-%d')
     response = supabase.from_('score').select('player_id, value').gte('date', today + ' 00:00:00').lte('date', today + ' 23:59:59').order('value', desc=True).execute()
@@ -34,6 +35,25 @@ def leaderboard():
 
     return leaderboard
 
+@app.route('/scores', methods=['POST'])
+def score():
+    try:
+        data = request.get_json()
+
+        if not data or 'game_id' not in data or 'player_id' not in data or 'value' not in data:
+            return jsonify({"error": "Dados inválidos"}), 400
+
+        response = (
+            supabase.table("score")
+            .insert(data)
+            .execute()
+        )
+
+        return jsonify({"message": "Pontuação inserida com sucesso", "data": response.data}), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 @app.route('/players')
 def players():
     response = supabase.table('player').select("*").order('name').execute()
@@ -41,3 +61,33 @@ def players():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+@app.route('/players', methods=['POST'])
+def players_insert():
+
+    try:
+        data = request.get_json()
+
+        if not data.get('name') or not isinstance(data['name'], str):
+            return jsonify({"error": "Nome inválido"}), 400
+
+        email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+        if not data.get('email') or not re.match(email_regex, data['email']):
+            return jsonify({"error": "Email inválido"}), 400
+
+        if not data.get('interest_area') or not isinstance(data['interest_area'], str):
+            return jsonify({"error": "Área de interesse inválida"}), 400
+
+        if not data.get('graduation_year') or not isinstance(data['graduation_year'], int):
+            return jsonify({"error": "Ano de graduação inválido"}), 400
+
+        response = (
+            supabase.table("player")
+            .insert(data)
+            .execute()
+        )
+
+        return jsonify({ "message": "Pontuação inserida com sucesso", "data": response.data}), 200
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
